4411cb0a110888658de2137169190ca4061b5d65
Ferdinand Lange <ferdinandlange@Ferdinands-MacBook-Pro.local>
2023-12-18 22:24:32 -0500

    flake8 added

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,2 +1,1 @@
 from .scripts.say_hello import main
-
--- hello/scripts/say_hello.py
+++ hello/scripts/say_hello.py
@@ -1,14 +1,18 @@
 from colorama import Fore, Style
 
+
 def say_hello():
     message = f"{Fore.RED}Hello!{Style.RESET_ALL}"
     print(message)
 
+
 if __name__ == "__main__":
     say_hello()
+
+
 def main():
     print("Hello!")
 
+
 if __name__ == '__main__':
     main()
-
